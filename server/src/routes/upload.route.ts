6980c64edcf90f24b48138b4f533851d2ca72a5d
Débora Camilly <debcamilly00@gmail.com>
2023-11-17 15:22:38 -0300

    Co-authored-by: Railson <Railson-Mateus@users.noreply.github.com>

--- server/src/routes/upload.route.ts
+++ server/src/routes/upload.route.ts
@@ -4,18 +4,18 @@ import { Request, Response, Router } from "express";
 
 const uploadRoute = Router();
 
-uploadRoute.post("/file", async (req: Request, res: Response) => {
+uploadRoute.post("/file", async (req: Request, res: Response) => {  
   await new Promise<void>((resolve, reject) => {
     upload.single("photo")(req, res, (err: any) => {
       if (err) {
-        console.log("resolve");
+        console.log("resolve", err);
         reject(new BadRequestError("Erro ao fazer upload da imagem"));
       } else {
         resolve();
       }
     });
   });
-
+  
   res.send(req.file ? req.file.filename : null);
 });
 
