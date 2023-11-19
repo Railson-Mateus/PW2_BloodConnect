612c8c13bcf7086c2c8be3b7db7c8d2096ae9073
Débora Camilly <87872533+deboraa07@users.noreply.github.com>
2023-11-19 16:54:21 -0300

    ajusta tela de perfil do usuario

---
+++ SessionService.ts
@@ -0,0 +1,47 @@
+import { NotFoundError, UnauthorizedError } from "@/helpers/api-erros";
+import { IService, ISessionService } from "@/interfaces/protocols";
+import { IUser } from "@/models/User";
+import { GenerateToken } from "@/provider/GenerateToken";
+import { RequestUser } from "@/validators/RequestUser";
+import { PrismaClient } from "@prisma/client";
+import { compare } from "bcrypt";
+
+type IResponse = {
+  token: string;
+  user: IUser;
+};
+
+export class SessionService implements IService<RequestUser, IResponse> {
+  constructor(
+    private readonly prisma: PrismaClient,
+    private readonly generateToken: GenerateToken
+  ) {}
+
+  async execute(data: RequestUser): Promise<IResponse> {
+    const { email, password } = data;
+    console.log("here", email);
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    
+    if (!user) {
+      throw new NotFoundError("User or Password incorrect!");
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new UnauthorizedError("User or Password incorrect!");
+    }
+
+    const userId = user.id;
+    delete user.password;
+
+    
+    const token = await this.generateToken.execute(userId, user.isAdmin);
+
+    return { token, user };
+  }
+}
---
+++ user.routes.ts
@@ -0,0 +1,125 @@
+import { Request, Response, Router } from "express";
+
+import { prisma } from "@/database/prismaClient";
+
+import { checkOwnership, ensuredAuthenticated, isAdmin } from "@/middlewares";
+
+import {
+  DeleteUserController,
+  GetAllUsersController,
+  GetDonationByDonorController,
+  GetLastDonationByUserController,
+  GetUserByIdController,
+  UpdateUserController,
+} from "@/controllers";
+
+import {
+  DeleteUserService,
+  GetAllUserService,
+  GetDonationsByDonorService,
+  GetLastDonationByUserService,
+  GetUserByIdService,
+  UpdateUserService,
+} from "@/services";
+
+const userRoutes = Router();
+
+userRoutes.patch(
+  "/update/:id",
+  ensuredAuthenticated(),
+  checkOwnership(),
+  async (req: Request, res: Response) => {
+    const updateUserService = new UpdateUserService(prisma);
+
+    const updateUserController = new UpdateUserController(updateUserService);
+
+    const result = await updateUserController.handle(req);
+
+    res.status(201).json(result);
+  }
+);
+
+userRoutes.delete(
+  "/delete/:id",
+  ensuredAuthenticated(),
+  checkOwnership(),
+  async (req: Request, res: Response) => {
+    const deleteUserService = new DeleteUserService(prisma);
+
+    const deleteUserController = new DeleteUserController(deleteUserService);
+
+    console.log(req.params);
+
+    const result = await deleteUserController.handle(req);
+
+    res.json(result);
+  }
+);
+
+userRoutes.get(
+  "/",
+  // ensuredAuthenticated(),
+  // isAdmin(),
+  async (req: Request, res: Response) => {
+    const getAllUserService = new GetAllUserService(prisma);
+
+    const getAllUserController = new GetAllUsersController(getAllUserService);
+
+    const result = await getAllUserController.handle();
+
+    res.status(201).json(result);
+  }
+);
+
+userRoutes.get(
+  "/:id",
+  ensuredAuthenticated(),
+  isAdmin(),
+  async (req: Request, res: Response) => {
+    const getUserService = new GetUserByIdService(prisma);
+
+    const getUserController = new GetUserByIdController(getUserService);
+
+    const result = await getUserController.handle(req);
+
+    res.status(201).json(result);
+  }
+);
+
+userRoutes.get(
+  "/:id/donations",
+  ensuredAuthenticated(),
+  checkOwnership(),
+  async (req: Request, res: Response) => {
+    const getDonationsByDonorService = new GetDonationsByDonorService(prisma);
+
+    const getDonationByDonorController = new GetDonationByDonorController(
+      getDonationsByDonorService
+    );
+
+    const result = await getDonationByDonorController.handle(req);
+
+    res.status(201).json(result);
+  }
+);
+
+userRoutes.get(
+  "/:id/latest-donation",
+  ensuredAuthenticated(),
+  checkOwnership(),
+  async (req: Request, res: Response) => {
+    const getLastDonationByUserService = new GetLastDonationByUserService(
+      prisma
+    );
+
+    const getLastDonationByUserController = new GetLastDonationByUserController(
+      getLastDonationByUserService
+    );
+
+    const result = await getLastDonationByUserController.handle(req);
+
+    res.status(201).json(result);
+  }
+);
+
+export { userRoutes };
