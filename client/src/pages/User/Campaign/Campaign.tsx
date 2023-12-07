991d0ac149f46e0620e41c67a66cfee04a685707
Débora Camilly <debcamilly00@gmail.com>
2023-12-07 15:46:54 -0300

    fix:ajusta cards de campanha

--- client/src/pages/User/Campaign/Campaign.tsx
+++ client/src/pages/User/Campaign/Campaign.tsx
@@ -1,36 +1,52 @@
 import { useAuth } from "@/hooks/useAuth" 
 import {api} from "@/api/axios"
 import { Box, Typography, Card, CardContent, 
-CardActions, Button} from "@mui/material"
+CardActions, Button, CardMedia, Modal} from "@mui/material"
 import { useEffect, useState } from "react";
 import { useTheme } from "@emotion/react";
 import campaign from "../../../assets/campaign.png"
 import PlaceIcon from '@mui/icons-material/Place';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
 
 const Campaign = () => {
     const { user } = useAuth()
 return (
     <Box>
-    <Box sx={{marginLeft:15, marginTop:2, width:130, marginBottom:0}}>
-    <img src={campaign} alt="imagem de campanha"></img>
+    <Box sx={{ marginLeft: 15, marginTop: 2, width: 130, marginBottom: 0 }}>
+      <img src={campaign} alt="imagem de campanha"></img>
     </Box>
-    <Typography sx={{fontSize:40, color:"white",fontStyle:"bold",textAlign:"center", marginLeft:40,marginTop:-10}}>
-        DOE RECOMEÇOS, DOE SANGUE!
+
+    <AddCircleIcon sx={{float:"right"}}/>
+
+    <Typography sx={{ fontSize: 40, color: "white", fontStyle: "bold", textAlign: "center", marginLeft: 40, marginTop: -10 }}>
+      DOE RECOMEÇOS, DOE SANGUE!
     </Typography>
-    <Card sx={{width: 500, height: 250,alignItems:"center", marginTop:5,marginLeft:15,bgcolor:"rgba(217,217,217,0.4)",
-    borderRadius:5,border:"solid", borderColor:"white"}}>
-      <CardContent sx={{marginTop:3, textAlign:"right"}}>
-        <Typography variant="h5" component="div" sx={{fontSize:20}}>
-        <CalendarMonthIcon></CalendarMonthIcon>27 de setembro das 10h às 11h
+
+  <Box>
+  <Card sx={{width: 500,height:300, marginLeft:20, marginTop:10 }}>
+    <DeleteIcon  sx={{float:"right", marginTop:1}}/>
+    <CreateIcon sx={{float:"right", marginTop:1}}/>
+      <CardMedia 
+        component="img"
+        sx={{marginTop:2,objectFit:"contain", height: 140 }}
+        image="./src/assets/campaign.png"
+        title="imagem-hemonucleo"
+        >
+      </CardMedia>
+      <CardContent sx={{display:"flex", flexDirection:"column"}}>
+        <Typography gutterBottom variant="h5" component="div" sx={{textAlign:"center"}}>
+          Hemonúcleo de Cajazeiras
         </Typography>
-        <Typography variant="h5" component="div" marginTop={1} sx={{fontSize:20}}>
-        <PlaceIcon></PlaceIcon>Bairro Centro, no hemonúcleo <br></br> de Cajazeiras - PB
+        <Typography variant="body2" color="text.secondary">
+          27 de setembro das 10h às 11h,
+          Bairro Centro, no hemonúcleo de Cajazeiras - PB
         </Typography>
       </CardContent>
-      <CardActions>
-      </CardActions>
     </Card>
+    </Box>
     </Box>
 )
 };
